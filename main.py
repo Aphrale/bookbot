2fccfdec0403b707aa6effd0d5367960f68d9bc1
Aphrale <rowloch@gmail.com>
2025-01-21 20:41:18 +0000

    word count

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(file_contents)
+
+def count_words():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        word_list = file_contents.split()
+        num_words = len(word_list)
+        print(num_words)
+
+main()
+count_words()
