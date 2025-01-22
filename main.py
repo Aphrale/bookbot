f4fe5e61866e100730ad7fe031718d9d2244e6c0
Aphrale <rowloch@gmail.com>
2025-01-22 08:54:21 +0000

    char count

--- main.py
+++ main.py
@@ -10,5 +10,15 @@ def count_words():
         num_words = len(word_list)
         print(num_words)
 
+def count_chars():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        num_chars = {}
+        for char in file_contents:
+            num_chars[char]=num_chars.setdefault(char, 0)+1
+            print(num_chars)
+
+
 main()
 count_words()
+count_chars()
