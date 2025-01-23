1f3489c2ad979ea52c04120afbccb62cf8105fb1
Aphrale <rowloch@gmail.com>
2025-01-23 20:10:08 +0000

    updated report

--- main.py
+++ main.py
@@ -17,11 +17,9 @@ def count_chars():
         file_contents = file_contents.lower()
         file_contents = "".join(file_contents.split())
         for char in file_contents:
-            if  if char == " " or char == "'" or char == "," or char == "(" or char == ")" or char == " " or char == "." or char == "-" or char == ":" or char == "1" or char == "2" or char == "3" or char == "4" or char == "5" or char == "6" or char == "7" or char == "8" or char == "9" or char == "0" or char == "[" or char == "]" or char == "#" or char == "*" or char == "?" or char == ";" or char == "!" or char == '"' or char == "_" or char == "/" or char == "%" or char == "@" or char == "$": 
-                del char
-            elif char in num_chars:
-                num_chars[char] = num_chars[char] + 1
-            else:
+            if char.isalpha() and char in num_chars:
+                    num_chars[char] = num_chars[char] + 1
+            elif char.isalpha():
                 num_chars[char] = 1
         for key in num_chars:
             print(f"The '" + key + "' character was found", num_chars[key], "times")
